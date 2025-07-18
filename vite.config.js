ef069b0b9c5d195ca51becee8806e9af2016991d
RAFE-00 <mohammedabdurrafem@gmail.com>
2025-07-18 14:53:50 +0530

    fix: jsx build issue

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // <- This is important for Vercel or GitHub Pages
-})
+});
